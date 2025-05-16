72143f5dcc68a26576379651c45327c9149eeb06
Richie Leonardo <richie.leonardoo@gmail.com>
2025-05-16 13:59:03 +0700

    change webpack.dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,20 +17,12 @@ module.exports = merge(common, {
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
-    port: 3000,
+    port: 9000,
     client: {
       overlay: {
         errors: true,
         warnings: true,
       },
-    },
-    server: {
-      type: 'https',
-      // For custom certificates (optional):
-      // options: {
-      //   key: fs.readFileSync(path.join(__dirname, 'path/to/server.key')),
-      //   cert: fs.readFileSync(path.join(__dirname, 'path/to/server.crt')),
-      // }
     },
   },
 });
